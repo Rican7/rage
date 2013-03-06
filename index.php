966e0d9db7b60c8162a28c0e1d0897897e4d8c06
Trevor Suarez <rican7@gmail.com>
2013-03-06 02:12:59 -0500

    Setting up our app's main route and response. :)

--- index.php
+++ index.php
@@ -3,10 +3,27 @@
 // Require our Composer-generated auto-loader
 require_once( __DIR__ . '/vendor/autoload.php' );
 
-$app = new \Paulus\Paulus();
+use \Paulus\Paulus;
+use \Paulus\Router;
+use \Predis\Client;
 
-\Paulus\Router::any( '/?', function( $request, $response, $service ) {
-	return $response->data = 'works!';
+$app = new Paulus();
+
+// Respond to a base-call
+Router::any( '/?', function( $request, $response, $service ) {
+	// Create a new Predis Client (connect)
+	$redis = new Client( array(
+		'host' => '127.0.0.1',
+		'port' => 6379
+	));
+
+	// Increment our number of given "fucks"
+	$fucks_given = $redis->incr('fucks');
+	
+	// Set the response data as an object
+	$response->data = (object) array(
+		'fucks_given' => $fucks_given,
+	);
 });
 
 $app->run();
