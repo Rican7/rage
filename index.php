6b73ed9b28a1bbf04530c4701a74fcde57dfb177
Trevor Suarez <rican7@gmail.com>
2013-03-06 02:34:57 -0500

    So, uh... yea. That's my app.

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ $app = new Paulus();
 
 // Respond to a base-call
 Router::any( '/?', function( $request, $response, $service ) {
+	// Grab our format param
+	$format = $request->param( 'format', 'json' );
+
 	// Create a new Predis Client (connect)
 	$redis = new Client( array(
 		'host' => '127.0.0.1',
@@ -18,12 +21,34 @@ Router::any( '/?', function( $request, $response, $service ) {
 	));
 
 	// Increment our number of given "fucks"
-	$fucks_given = $redis->incr('fucks');
+	$fucks_given = $redis->incr('app:rage fucks');
 	
-	// Set the response data as an object
-	$response->data = (object) array(
-		'fucks_given' => $fucks_given,
-	);
+	// Depending on the format..
+	switch ( $format ) {
+		case 'json' :
+			// Set the response data as an object
+			$response->data = (object) array(
+				'fucks_given' => $fucks_given,
+			);
+			break;
+
+		case 'plain' :
+			// Return as plain-text
+			echo $fucks_given . ' fucks given';
+			exit;
+			break;
+
+		default :
+			// Return an error
+			$service->app->abort( 400, 'INVALID_FORMAT', 'Invalid format type', array(
+				'valid_formats' => array(
+					'json',
+					'plain'
+				),
+			));
+			break;
+	}
+
 });
 
 $app->run();
